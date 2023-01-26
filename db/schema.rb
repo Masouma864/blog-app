0843f45648ce8c77cf73f0194b005d9ed6d1a9f4
masouma <masouma2020@gmail.com>
2023-01-26 13:37:03 +0430

    create post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_090328) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_090628) do
   create_table "comments", force: :cascade do |t|
     t.string "authorId"
     t.text "postId"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "authorId"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "like_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
