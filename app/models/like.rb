9b1e3bbd9141c7c110cb08f0d54e2a2189d4f413
masouma <masouma2020@gmail.com>
2023-01-29 00:17:03 +0430

    set validation for like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :like_counter
 
+  
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  
   private
 
   def like_counter
