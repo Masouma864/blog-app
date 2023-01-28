7ad8cfa2cf97202045b3ae8adb79bdd1c02824bd
masouma <masouma2020@gmail.com>
2023-01-28 14:42:36 +0430

    resolve linter error

--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_070518) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.integer "comment_counter"
-    t.integer "like_counter"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
+ActiveRecord::Schema[7.0]
+  .define(version: 20_230_128_070_518) do
+  enable_extension 'plpgsql'
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'text'
+    t.integer 'comment_counter'
+    t.integer 'like_counter'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_articles_on_user_id'
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text "text"
-    t.integer "comment_counter"
-    t.bigint "user_id", null: false
-    t.bigint "posts_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table 'comments', force: :cascade do |t|
+    t.text 'text'
+    t.integer 'comment_counter'
+    t.bigint 'user_id', null: false
+    t.bigint 'posts_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['posts_id'], name: 'index_comments_on_posts_id'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.integer "comment_counter"
-    t.integer "like_counter"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'text'
+    t.integer 'comment_counter'
+    t.integer 'like_counter'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_posts_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.text "bio"
-    t.integer "PostsCounter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'photo'
+    t.text 'bio'
+    t.integer 'PostsCounter'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "articles", "users"
-  add_foreign_key "comments", "posts", column: "posts_id"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key 'articles', 'users'
+  add_foreign_key 'comments', 'posts', column: 'posts_id'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'posts', 'users'
 end
