f3165cb6c05c2ef59bae956fbb670bf66ef3046c
masouma <masouma2020@gmail.com>
2023-01-29 01:08:06 +0430

    set rspec test

--- Gemfile
+++ Gemfile
@@ -71,3 +71,7 @@ group :test do
   gem 'webdrivers'
 end
 gem 'rubocop', '>= 1.0', '< 2.0'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,9 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :comment_counter
 
-  
+
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  
+
   private
 
   def comment_counter
