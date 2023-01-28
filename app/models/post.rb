c1f8df44e3f2812cc4614d3b376930e4b57cd1a0
masouma <masouma2020@gmail.com>
2023-01-28 23:52:23 +0430

    add post model with association to author

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+    has_many :likes, foreign_key: 'posts_id'
+    has_many :comments, foreign_key: 'posts_id'
+    belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
+    before_save :post_counter
+  
+    def latest_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+  
+    private
+    def post_counter
+      author.update(posts_counter: author.posts.all.length)
+    end
+  end
