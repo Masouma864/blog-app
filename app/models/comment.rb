c5f9a47ce344b207c5056e69fde8f36ef39d7439
masouma <masouma2020@gmail.com>
2023-01-29 01:15:51 +0430

    resolve linter error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :comment_counter
 
-
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
 
   private
