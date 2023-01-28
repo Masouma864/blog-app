34b1e2add1f0287f2dbf71dd5e8643d75c904da8
masouma <masouma2020@gmail.com>
2023-01-28 23:51:11 +0430

    Add like model with associaton to post and user

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+    belongs_to :post
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    before_save :like_counter
+
+  private
+    def like_counter
+      post.update(likes_counter: post.likes.all.length)
+    end
+end
