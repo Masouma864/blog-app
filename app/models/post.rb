cb07a5c7aa450f4f95b1fcca2c16fd0e2f341a93
masouma <masouma2020@gmail.com>
2023-01-29 02:19:01 +0430

    change test files

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
   after_save :post_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def latest_comments
     comments.order(created_at: :desc).limit(5)
