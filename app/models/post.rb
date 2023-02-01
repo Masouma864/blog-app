725f70070d34cbad20c46abb275c402dd7aacb3f
masouma <masouma2020@gmail.com>
2023-02-01 11:08:31 +0430

    resolve linter error

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,6 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User'
   after_save :post_counter
 
-
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
