29c0edb8cf6d42743b87b09bd0ef0d30a9637e85
masouma <masouma2020@gmail.com>
2023-02-01 11:30:01 +0430

    Use resourse in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'posts/index'
-  get 'posts/show'
-  get 'comments/index'
-  get 'comments/show'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+    resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+ 
+  end
+
 end
