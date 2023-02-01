9329679edbbdb9ccb589ee90c47ff3c6609e8a70
masouma <masouma2020@gmail.com>
2023-02-01 18:06:49 +0430

    Refactor codes

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
     context 'when page is opened ' do
       it 'return a correct response' do
-        get '/users/:user_id/posts''
+        get '/users/:user_id/posts'
         expect(response).to have_http_status(200)
         expect(response).to render_template(:index)
         expect(response.body).to include('Lists all posts for Joseph')
       end
     end
-  
-    describe 'GET /show' do
-      context 'when page is opened ' do
-        it 'return a correct response' do
-          get '/users/:user_id/posts/:id'
-          expect(response).to have_http_status(200)
-          expect(response).to render_template(:show)
-          expect(response.body).to include('Shows info for Joseph posts')
-        end
+  end
+
+  describe 'GET /show' do
+    context 'when page is opened ' do
+      it 'return a correct response' do
+        get '/users/:user_id/posts/:id'
+        expect(response).to have_http_status(200)
+        expect(response).to render_template(:show)
+        expect(response.body).to include('Shows info of Joseph posts')
       end
     end
   end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
     context 'when page is opened ' do
-      it 'return a correct response' do
+      it 'has to return a correct response' do
         get '/users'
         expect(response).to have_http_status(200)
         expect(response).to render_template(:index)
-        expect(response.body).to include('Lists all posts for Joseph')
-      end
-    end
-  
-    describe 'GET /show' do
-      context 'when page is opened ' do
-        it 'return a correct response' do
-          get  '/users/:id''
-          expect(response).to have_http_status(200)
-          expect(response).to render_template(:show)
-          expect(response.body).to include('Shows info for Joseph posts')
-        end
+        expect(response.body).to include('users')
       end
     end
   end
+
+  describe 'GET /show' do
+    context 'when page is open' do
+      it 'has to return a correct response' do
+        get '/users/:id'
+        expect(response).to have_http_status(200)
+        expect(response).to render_template(:show)
+        expect(response.body).to include('Show list of all users')
+      end
+    end
+  end
+end
