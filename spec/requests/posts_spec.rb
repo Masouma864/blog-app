a34fb261a939f04a83f0dfcf332288b7fa36aebb
masouma <masouma2020@gmail.com>
2023-02-01 17:06:50 +0430

    Add all the test for user request

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Posts", type: :request do
   describe "GET /index" do
     context 'when page is opened ' do
       it 'return a correct response' do
-        get '/users/:user_id/posts'
+        get '/users/:user_id/posts''
         expect(response).to have_http_status(200)
         expect(response).to render_template(:index)
         expect(response.body).to include('Lists all posts for Joseph')
