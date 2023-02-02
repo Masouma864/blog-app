d48e55456de6a5648e0ae20fe1bf74ac92e020ee
masouma <masouma2020@gmail.com>
2023-02-02 15:01:11 +0430

    Create standard file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_174555) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_093938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,12 +41,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_174555) do
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
     t.index ["post_id"], name: "index_posts_on_post_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,5 +64,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_174555) do
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "posts"
-  add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
