bf5607f3d1cf0ce1908c1e4cb8f3bd56b509dc3a
masouma <masouma2020@gmail.com>
2023-02-02 15:05:20 +0430

    Setup methods in postController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
-  def index; end
+  layout "standard"
 
-  def show; end
-end
+  before_action :set_user
+
+  def index
+    @posts = @user.posts
+  end
+
+  def show
+    @post = @user.posts.find(params[:post_id])
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
